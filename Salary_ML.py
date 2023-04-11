943440898c4226df71149655deae8369a66c01c0
sschangi <schangiz2002@gmail.com>
2023-04-10 22:09:02 -0700

    add a minor comment

--- Salary_ML.py
+++ Salary_ML.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
-
+# clean data
 def clean_data(df):
     '''
     INPUT
